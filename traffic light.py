02ab125b96b281e336a8a0ef32d4d71292473434
CODEwithASHISH47 <affairs24678@gmail.com>
2026-08-27 18:16:56 +0530

    Refactor traffic light code for improved readability

--- traffic light.py
+++ traffic light.py
@@ -1,13 +1,13 @@
 #traffic light code
 
 light = input("light : ")
-if(light == "red"):
- print("stop")
-elif(light == "yellow"):
-   print("look around")
-elif(light == "green"):
+if light == "red":
+    print("stop")
+elif light == "yellow":
+    print("look around")
+elif light == "green":
     print("go ahead")
-elif(light == "brown, black,violet"):
+elif light in ["brown","black","violet"]:
     print("the light is failed and broken")
 else:
- print("get attention on yourself:")
+     print("get attention on yourself:")
