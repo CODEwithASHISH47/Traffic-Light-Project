21609b17afa8ec378094fd3f40ffbc9f83413d96
CODEwithASHISH47 <affairs24678@gmail.com>
2026-08-27 17:58:27 +0530

    Add files via upload
    a traffic light project with some codes by a beginner programmer

---
+++ traffic light.py
@@ -0,0 +1,13 @@
+#traffic light code
+
+light = input("light : ")
+if(light == "red"):
+ print("stop")
+elif(light == "yellow"):
+   print("look around")
+elif(light == "green"):
+    print("go ahead")
+elif(light == "brown, black,violet"):
+    print("the light is failed and broken")
+else:
+ print("get attention on yourself:")
